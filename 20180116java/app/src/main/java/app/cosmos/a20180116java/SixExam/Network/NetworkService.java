0ddd7f1184866c511c9fcb801e42a814634c8751
DESKTOP-AGPEQ28\sec <jhsw0375@gmail.com>
2018-02-08 00:16:11 +0900

    통신 시 사용되는 메소드들 주석 추가

--- 20180116java/app/src/main/java/app/cosmos/a20180116java/SixExam/Network/NetworkService.java
+++ 20180116java/app/src/main/java/app/cosmos/a20180116java/SixExam/Network/NetworkService.java
@@ -18,8 +18,16 @@ import retrofit2.http.Path;
 
 public interface NetworkService {
 
+    /*FIXME
+    *GET : 주로 정보를 요청하기 위해서 사용
+    *POST : 서버에 정보를 저장하기 위해서 사용
+    *PUT : 정보를 업데이트하기 위해서 사용
+    *DELETE : 정보를 삭제하기 위해서 사용
+    **/
     @GET("/")
     Call<MainResult> getMainResult();
+    // getMainResult()라는 함수를 호출하고
+    // 서버로부터 정보(응답)를(을) 받을 때는 Call을 호출하고 MainResult 형태로 받겠다.
 
     @GET("/{id}")
     Call<DetailResult> getDetailResult(@Path("id") String id);
