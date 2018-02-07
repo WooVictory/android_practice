ff5c2518c481e0b206844b11af9b54f84e3b8398
DESKTOP-AGPEQ28\sec <jhsw0375@gmail.com>
2018-02-07 20:28:27 +0900

    주석 내용 추가

--- 20180116java/app/src/main/java/app/cosmos/a20180116java/Exam/MoneyUnitActivity.java
+++ 20180116java/app/src/main/java/app/cosmos/a20180116java/Exam/MoneyUnitActivity.java
@@ -18,6 +18,8 @@ public class MoneyUnitActivity extends AppCompatActivity {
     @BindView(R.id.wonEdit)
     EditText wonEdit;
     private DecimalFormat decimalFormat = new DecimalFormat("#,###");
+    // 이 부분이 EditText에 입력되는 부분을 원화 단위로 글자를 변경시켜주는 부분이다.
+    // 소수점이나 다른 단위가 필요하면 이 부분을 변경하면 된다.
     private String result="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,9 @@ public class MoneyUnitActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         wonEdit.addTextChangedListener(new TextWatcher() {
+            /*FIXME
+            * EditText의 글자 변화를 감지한다.
+            * */
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
@@ -35,6 +40,13 @@ public class MoneyUnitActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 if(!TextUtils.isEmpty(charSequence.toString())&& !charSequence.toString().equals(result))
                 {
+                    /*FIXME
+                    !TextUtils.isEmpty(charSequence.toString()) 이 부분은 EditText가 비어있지 않을 때만 실행한다는 것입니다.
+                    !charSequence.toString().equals(result) 이 조건은
+                    TextWatcher는 계속해서 EditText를 검사하게 되는데, 그 때마다 메소드를 반복 실행합니다.
+                    아직 변경이 된 부분이 없는데, 반복적으로 포맷 변경을 하고 setText를 하는 동작은 불필요하기 때문에
+                    이전의 결과값과 동일하면 실행시키지 않습니다. 
+                    * */
                     result = decimalFormat.format(Double.parseDouble(charSequence.toString().replaceAll(",","")));
                     wonEdit.setText(result);
                     wonEdit.setSelection(result.length());
