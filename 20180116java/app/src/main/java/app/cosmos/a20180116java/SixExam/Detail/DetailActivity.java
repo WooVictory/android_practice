0baebcc306ab229ceac96cf22abea21a6f15de46
DESKTOP-AGPEQ28\sec <jhsw0375@gmail.com>
2018-02-15 21:41:11 +0900

    레이아웃 변경된 사항을 적용 그리고 댓글 등록 통신부 추가

--- 20180116java/app/src/main/java/app/cosmos/a20180116java/SixExam/Detail/DetailActivity.java
+++ 20180116java/app/src/main/java/app/cosmos/a20180116java/SixExam/Detail/DetailActivity.java
@@ -6,8 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -33,6 +35,12 @@ public class DetailActivity extends AppCompatActivity {
     public ImageView imgView;
     @BindView(R.id.closeBtn)
     public Button closeBtn;
+    @BindView(R.id.commentBtn)
+    Button commentBtn;
+    @BindView(R.id.comment_writer_edit)
+    EditText comment_writer_edit;
+    @BindView(R.id.comment_content_edit)
+    EditText comment_content_edit;
     private String id;
     private NetworkService networkService;
     @Override
@@ -53,13 +61,44 @@ public class DetailActivity extends AppCompatActivity {
 
     }
 
-    @OnClick(R.id.closeBtn)
+    @OnClick({R.id.closeBtn,R.id.commentBtn})
     void click(View view)
     {
         switch (view.getId())
         {
             case R.id.closeBtn:
                 finish();
+                break;
+            case R.id.commentBtn:
+                final CommentInfo commentInfo = new CommentInfo();
+                commentInfo.writer = comment_writer_edit.getText().toString();
+                commentInfo.content = comment_content_edit.getText().toString();
+                if(commentInfo.writer.length() == 0 || commentInfo.content.length() == 0)
+                {
+                    Toast.makeText(getApplicationContext(), "댓글 작성자 혹은 내용을 입력해주세요.",Toast.LENGTH_LONG).show();
+                }else {
+                    Call<CommentResult> commentResultCall = networkService.getCommentResult(id, commentInfo);
+                    commentResultCall.enqueue(new Callback<CommentResult>() {
+                        @Override
+                        public void onResponse(Call<CommentResult> call, Response<CommentResult> response) {
+                            if(response.isSuccessful())
+                            {
+
+                                if (response.body().message.equals("ok")) {
+                                    Toast.makeText(getApplicationContext(),"댓글 등록 성공",Toast.LENGTH_LONG).show();
+                                    comment_writer_edit.setText("");
+                                    comment_content_edit.setText("");
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<CommentResult> call, Throwable t) {
+                            Toast.makeText(getApplicationContext(),"댓글 등록 실패! err message : "+t.getMessage(),Toast.LENGTH_LONG).show();
+
+                        }
+                    });
+                }
                 break;
         }
     }
@@ -74,19 +113,19 @@ public class DetailActivity extends AppCompatActivity {
             public void onResponse(Call<DetailResult> call, Response<DetailResult> response) {
                 if(response.isSuccessful())
                 {
-                    titleTextView.setText("제목 :"+response.body().result.title);
-                    writerTextView.setText(response.body().result.username);
-                    contentTextView.setText("내용 :"+response.body().result.content);
+                    titleTextView.setText("제목 :"+response.body().result.post.title);
+                    writerTextView.setText(response.body().result.post.username);
+                    contentTextView.setText("내용 :"+response.body().result.post.content);
                     /*FIXME
                     값들을 받아서 텍스트뷰에 뿌려준다.
                     그리고 서버에서 받아온 이미지가 null이 아니면 즉, 값이 존재하면
                     Glide를 통해서 이미지를 imgView에 넣어준다.
                     * */
 
-                    if(response.body().result.image !="")
+                    if(response.body().result.post.image !="")
                     {
                         Glide.with(getApplicationContext())
-                                .load(response.body().result.image)
+                                .load(response.body().result.post.image)
                                 .into(imgView);
                     }
                 }
