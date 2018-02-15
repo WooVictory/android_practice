5c2a72b362c4271433fe293e224c14d357999621
DESKTOP-AGPEQ28\sec <jhsw0375@gmail.com>
2018-02-15 21:40:33 +0900

    DetailResult 데이터 클래스 수정

--- 20180116java/app/src/main/java/app/cosmos/a20180116java/SixExam/Detail/DetailResult.java
+++ 20180116java/app/src/main/java/app/cosmos/a20180116java/SixExam/Detail/DetailResult.java
@@ -1,17 +1,30 @@
 package app.cosmos.a20180116java.SixExam.Detail;
 
+import java.util.ArrayList;
+
 /**
  * Created by sec on 2018-01-26.
  */
 
 public class DetailResult {
-    public DetailData result;
+    public ResultData result;
+    public String message;
+
+    public class ResultData{
+        public DetailData post;
+        public ArrayList<CommentData> comment;
+    }
 
     class DetailData{
         public int id;
         public String username;
         public String title;
         public String image;
+        public String content;
+    }
+    class CommentData{
+        public String writer;
+        public String written_time;
         public String content;
     }
 }
