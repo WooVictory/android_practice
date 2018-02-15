276284788acfd261ccfa3be8879a9bb7ab42dba6
DESKTOP-AGPEQ28\sec <jhsw0375@gmail.com>
2018-02-15 22:38:21 +0900

    MainListData 변수 추가

--- 20180116java/app/src/main/java/app/cosmos/a20180116java/SixExam/Main/MainListData.java
+++ 20180116java/app/src/main/java/app/cosmos/a20180116java/SixExam/Main/MainListData.java
@@ -7,6 +7,8 @@ package app.cosmos.a20180116java.SixExam.Main;
 public class MainListData {
     // 데이터 클래스
     int id;
-    String username;
+    int view_number;
+    String writer;
     String title;
+    String written_time;
 }
