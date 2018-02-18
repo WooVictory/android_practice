aad7cf4433ee27a5045d0a6d0184b6af3b34bad6
DESKTOP-AGPEQ28\sec <jhsw0375@gmail.com>
2018-02-18 20:26:51 +0900

    RecyclerActivity 파일 추가

---
+++ 20180116java/app/src/main/java/app/cosmos/a20180116java/EightExam/RecyclerActivity.java
@@ -0,0 +1,15 @@
+package app.cosmos.a20180116java.EightExam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import app.cosmos.a20180116java.R;
+
+public class RecyclerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+    }
+}
